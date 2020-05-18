7f74c26bf32739ed03843db70cf19655a8d301a1
David Ortega <daorte@Kakarotto.local>
2020-05-18 15:25:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AWS SAM simple microservice template
---
+++ function-retrieve-by-id/retrieve-by-id.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let response;
+const AWS = require('aws-sdk');
+AWS.config.update({ region: process.env.Region });
+if (process.env.AWS_SAM_LOCAL) AWS.config.update({ dynamodb: { endpoint: process.env.DynamoDbEndpoint } });
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.handler = async (event, context) => {
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'RETRIEVE BY ID'
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
